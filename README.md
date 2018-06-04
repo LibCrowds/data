c35e20c3bd4af5f9418ddd91503fba4df39f96d9
Alexander Mendes <alexanderhmendes@gmail.com>
2018-06-04 11:31:07 +0100

    Add get tasks script

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ pip install -r requirements.txt
 
 ### Usage
 
-The repository contains the following scripts, all of which will output data to
-the /data folder.
+The repository contains the following scripts, all of which will output data
+to the /data folder.
 
 ## MARC Template transformation
 
@@ -19,7 +19,7 @@ Download all Playbills results data and transform into the format required
 for ingest by metadata services.
 
 ```
-python marc-tmpl.py
+python bin/to-marc-csv.py
 ```
 
 ## Transcriptions to CSV
@@ -29,5 +29,15 @@ CSV files with values against Annotation IDs for each tag type
 (title, genre, date etc.).
 
 ```
-python transcriptions-to-csv.py
+python bin/transcriptions-to-csv.py
+```
+
+## Download PYBOSSA tasks into a dataframe
+
+The functions here are used within other scripts in this repository that
+require access to all of the PYBOSSA task data. It can also be run as a
+standalone script, in which case the tasks will be output to CSV.
+
+```
+python bin/get-tasks.py
 ```
---
+++ bin/get-tasks.py
@@ -0,0 +1,74 @@
+# -*- coding: utf8 -*-
+"""
+Download all PYBOSSA tasks and load into a dataframe.
+
+If run from as a standalone script the dataframe is saved as a CSV.
+"""
+import tqdm
+import time
+import pandas
+import requests
+import datetime
+
+
+
+BASE_URL = 'https://backend.libcrowds.com'
+
+
+def get_n_tasks():
+    """Get the number of tasks on the server."""
+    r = requests.get(BASE_URL + '/stats/', headers={
+      'content-type': 'application/json'
+    })
+    r.raise_for_status()
+    n_tasks = r.json()['stats']['n_tasks']
+    return n_tasks
+
+
+def get_tasks(offset=0):
+    """Get a set of domain objects."""
+    r = requests.get(BASE_URL + '/api/task', params={
+        'offset': offset,
+        'limit': 100,
+        'all': 1
+    })
+    r.raise_for_status()
+    return r
+
+
+def get_tasks_df():
+    """Load all of the chosen domain objects into a dataframe and return."""
+    n_tasks = get_n_tasks()
+    progress = tqdm.tqdm(desc='Downloading tasks', total=n_tasks, unit='tasks')
+    data = []
+    last_fetched = []
+    while _not_exhausted(last_fetched):
+        r = get_tasks(len(data))
+        last_fetched = r.json()
+        data += last_fetched
+        n_fetched = len(last_fetched)
+        progress.update(n_fetched)
+        respect_rate_limits(r)
+    progress.close()
+    df = pandas.DataFrame(data)
+    return df
+
+
+def _not_exhausted(last_fetched):
+    """Check if the last fetched tasks were the last available."""
+    return len(last_fetched) == 0 or len(last_fetched) == 100
+
+
+def respect_rate_limits(response):
+    """If we have exceeded the rate limit sleep until it is refreshed."""
+    reset = response.headers['x-ratelimit-reset']
+    reset_dt = datetime.datetime.fromtimestamp(float(reset))
+    remaining = response.headers['x-ratelimit-remaining']
+    if remaining == 0:
+        while reset_dt > datetime.datetime.now():
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    df = get_tasks_df()
+    df.to_csv('../data/tasks.csv')
