1f4cfdb4b264c4028405295cc092bbee4ddccb0f
Alex Mendes <alexanderhmendes@gmail.com>
2018-07-03 16:24:54 +0100

    Drop duplicate references from Convert-a-Card index

--- scripts/generate_cac_index.py
+++ scripts/generate_cac_index.py
@@ -51,6 +51,7 @@ def get_cac_index_df():
     df['reference'] = df['task_id'].apply(lookup_reference,
                                           args=(reference_df,))
 
+    df.drop_duplicates(subset=['reference'], inplace=True)
     return df[['control_number', 'reference']]
 
 
